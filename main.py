c1a82f4ae26d499aa884ca7cc5bf08ed20a25041
Paulo Henrique <ph55@hotmail.com>
2024-04-09 18:40:55 -0300

    Update main.py
    add break para encerrar o loop
    add as bibliotecas os e time

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 from features import menu
-
-menu.home_menu()
-menu.options_menu()
-
+from time import sleep
+from os import system
 
 while True:
+    menu.home_menu()
+    menu.options_menu()
     try:
         select_user = int(input(''))
     except Exception as e:
-        print('Valor Inválido')
+        system('cls')
+        print('Valor Inválido!!')
+        sleep(1)
+        system('cls')
     else:
         if select_user == 1: # Cadastro de Cliente
             pass
+            break
         elif select_user == 2: # Cadastro de Veículo
             pass
+            break
         elif select_user == 3: # Cadastro de Reserva
             pass
+            break
         elif select_user == 4: # Cadastro de Transação
             pass
+            break
         elif select_user == 5: # Sair
             pass
-        else:
-            pass
+            break
+        else: # Valores fora do Range
+           system('cls')
+           print('Valor Inválido!!')
+           sleep(1)
+           system('cls')
