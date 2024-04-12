25c3a06d0960cb3a9db3520a48bac7da256ec7d6
Paulo Henrique <ph55@hotmail.com>
2024-04-12 00:27:48 -0300

    Update mysql-connector.py
    Add a biblioteca regex
    Add a função que pega o email

--- features/mysql_connector.py
+++ features/mysql_connector.py
@@ -2,10 +2,12 @@ import mysql.connector
 from datetime import date
 from time import sleep
 from os import system
+import re
 
 def get_first_name():
     while True:
         try:
+            system('cls')
             first_name = input("Primeiro Nome: ").strip() 
             if first_name:
                 return first_name
@@ -25,6 +27,7 @@ def get_first_name():
 def get_last_name():
     while True:
         try:
+            system('cls')
             last_name = input("Último Nome: ").strip() 
             if last_name:
                 return last_name
@@ -40,11 +43,25 @@ def get_last_name():
             sleep(1)
             system('cls')
 
+def get_email():
+    email_pattern = re.compile(r'^[\w\.-]+@[\w\.-]+\.\w+$')
 
-
-
-
-
+    while True:
+        try:
+            system('cls')
+            email = input("Email: ").strip() 
+            if email and email_pattern.match(email):
+                return email
+            else:
+                system('cls')
+                print("Valor Inválido")
+                sleep(1)
+                system('cls')
+        except Exception as e:
+            system('cls')
+            print("Valor Inválido")
+            sleep(1)
+            system('cls')
 
 
 
