61fbaaab2aeec62f6bd19a35fa6ac287aa831905
Paulo Henrique <ph55@hotmail.com>
2024-04-15 00:48:23 -0300

    Create veiculo_cadastro.py
    Criado a modulo responsável por conter as funções de cadastrar veículo
    Add as funções para cadastrar veículos

---
+++ features/veiculo_cadastro.py
@@ -0,0 +1,34 @@
+# vehicle registration
+from customer_registration import exit_except
+
+#Pega Marca
+def get_brand():
+    pass
+
+#Pega Modelo
+def get_model():
+    pass
+
+#Pega Ano
+def get_year():
+    pass
+
+#Pega Placa
+def get_plate():
+    pass
+
+#Pega Cor
+def get_color():
+    pass
+
+#Pega Tipo de Combustivel
+def get_type_fuel():
+    pass
+
+#Pega Disponibilidade
+def get_availabe():
+    pass
+
+#Pega Valor Diaria
+def get_daily_value():
+    pass
