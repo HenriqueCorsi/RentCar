32d4eea51ceca95857ec181bebd7d8ee8af17be0
Paulo Henrique <ph55@hotmail.com>
2024-04-16 20:44:57 -0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -7,7 +7,7 @@ def home_menu():
     print('================================')
 
 def options_menu():
-    options = ['1 -> Cadastrar Cliente', '2 -> Cadastrar Veículo', '3 -> Vizualizar Clientes', '4 -> Vizualzar Veículos', '5 -> Sair']
+    options = ['1 -> Cadastrar Cliente', '2 -> Cadastrar Veículo', '3 -> Cadastrar Reserva', '4 -> Vizualzar Clientes', '5 -> Vizualizar Veículos', '6 -> Vizualizar Reservas', '7 -> Sair']
 
     for x in options:
         print(x)
